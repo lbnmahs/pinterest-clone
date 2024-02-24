51d67c0493424099bce4a8a41bbbf09f3d5443f0
LbnMahs <labanmahs2002@gmail.com>
2024-02-24 11:15:54 +0300

    themes

--- lib/main.dart
+++ lib/main.dart
@@ -27,16 +27,12 @@ final theme = ThemeData(
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pinterest',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: theme,
       home: const Text('Flutter Demo Home Page'),
     );
   }
