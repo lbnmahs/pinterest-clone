55e1525a0f0fe63e03908f141ce7d8df6eaeaae1
LbnMahs <labanmahs2002@gmail.com>
2024-02-25 16:32:29 +0300

    images base styling

--- lib/views/screens/images_screen.dart
+++ lib/views/screens/images_screen.dart
@@ -17,11 +17,13 @@ class _ImagesScreenState extends State<ImagesScreen> {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
+      padding: const EdgeInsets.all(10),
       itemCount: widget.images.length,
       gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         crossAxisSpacing: 4.0,
         mainAxisSpacing: 4.0,
+        childAspectRatio: 0.8
       ), 
       itemBuilder: (context, index) => ImageItem(
         image: widget.images[index],
--- lib/views/widgets/image_item.dart
+++ lib/views/widgets/image_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pinterest_clone/models/image_model.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class ImageItem extends StatelessWidget {
   final GeneratedImage image;
@@ -9,18 +10,26 @@ class ImageItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         Expanded(
-          child: Image.network(
-            image.url,
-            fit: BoxFit.cover,
-            cacheHeight: 200,
-            cacheWidth: 200,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: FadeInImage(
+              placeholder: MemoryImage(kTransparentImage), 
+              image: NetworkImage(image.url),
+              fit: BoxFit.cover,
+              height: 200,
+              width: 200,
+            ),
           ),
         ),
         const Padding(
-          padding: EdgeInsets.all(10),
-          child: Icon(Icons.menu_rounded),
+          padding: EdgeInsets.only(top: 5, bottom: 5),
+          child: Icon(
+            Icons.menu_rounded,
+            size: 12,
+          ),
         )
       ],
     );
